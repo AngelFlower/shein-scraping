abc16509f337e520f5772026cbc12208ee20586c
Angel Flores Carlos <angelfc.contact@gmail.com>
2023-03-04 16:49:41 -0600

    add gSheets info route

--- src/controllers/product/googleSheets.js
+++ src/controllers/product/googleSheets.js
@@ -1,5 +1,44 @@
 import { sheinRequest } from "./request.js";
 
+export const getInformation = async (req, res) => {
+  try {
+    const { url, location } = req.query;
+
+    if (!url) {
+      return res.json({ message: "Please send a valid id" }).status(400);
+    }
+
+    const { title, id, html } = await sheinRequest(url, location);
+
+    const imgTags = html.match(/<img[^>]+src="([^">]+)/g);
+
+    const dataSrc = imgTags.map((imgTag) => {
+      const dataSrc = imgTag.match(/data-src="([^">]+)/g);
+      return `https:${dataSrc[0].replace('data-src="', "")}`;
+    });
+
+    const salePrice = html
+      .match(/"salePrice":(.*?),/g)[0]
+      .replace('"salePrice":', "")
+      .replace(",", "")
+      .split(":")[1]
+      .match(/"([^"]+)"/)[1];
+
+    const data = [
+      title,
+      id,
+      salePrice,
+      dataSrc[0],
+      dataSrc.slice(1).toString(),
+    ];
+
+    res.json(data).status(200);
+  } catch (error) {
+    console.log(error);
+    res.json({ message: "Error" }).status(500);
+  }
+};
+
 export const getPrincipalImage = async (req, res) => {
   try {
     const { url, location } = req.query;
--- src/routes/gsheets.routes.js
+++ src/routes/gsheets.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import {
+  getInformation,
   getPrincipalImage,
   getImages,
 } from "../controllers/product/googleSheets.js";
 
 const router = express.Router();
 
+router.get("/", getInformation);
 router.get("/first-img/", getPrincipalImage);
 router.get("/imgs/", getImages);
 
